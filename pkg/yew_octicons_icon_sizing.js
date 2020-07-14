71f1bd96ba9d1275ada93bbc55c62858d3cab0be
io12 <7348004+io12@users.noreply.github.com>
2020-07-14 23:08:03 +0000

    Deploying to gh-pages from  @ e277c263fc8ed00edfd6dace170dec4f5cb60939 🚀

--- pkg/yew_octicons_icon_sizing.js
+++ pkg/yew_octicons_icon_sizing.js
@@ -503,7 +503,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper2382 = logError(function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2457 = logError(function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 43, __wbg_adapter_16);
         return addHeapObject(ret);
     });
