d232718b227ee8cd679ccf13c186c10c96a109f8
io12 <7348004+io12@users.noreply.github.com>
2021-12-14 03:04:49 +0000

    Deploying to gh-pages from  @ 2fec55ed1f5cc11a1dbf40da6f7292a25b5eb6dc 🚀

--- pkg/yew_octicons_icon_sizing.d.ts
+++ pkg/yew_octicons_icon_sizing.d.ts
@@ -12,7 +12,7 @@ export interface InitOutput {
   readonly __wbindgen_malloc: (a: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly _dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h6045265752e062ae: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h698d14b6ed088110: (a: number, b: number, c: number) => void;
   readonly __wbindgen_free: (a: number, b: number) => void;
   readonly __wbindgen_exn_store: (a: number) => void;
   readonly __wbindgen_start: () => void;
--- pkg/yew_octicons_icon_sizing.js
+++ pkg/yew_octicons_icon_sizing.js
@@ -240,7 +240,7 @@ function logError(f, args) {
 function __wbg_adapter_18(arg0, arg1, arg2) {
     _assertNum(arg0);
     _assertNum(arg1);
-    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h6045265752e062ae(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h698d14b6ed088110(arg0, arg1, addHeapObject(arg2));
 }
 
 /**
@@ -521,7 +521,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper1997 = function() { return logError(function (arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1998 = function() { return logError(function (arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 73, __wbg_adapter_18);
         return addHeapObject(ret);
     }, arguments) };
