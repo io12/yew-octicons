e8367330d5acea0c8e27a54e4fc225455a2c8f27
io12 <7348004+io12@users.noreply.github.com>
2021-12-14 08:43:06 +0000

    Deploying to gh-pages from  @ d12d9b15afa077b8374890c8baccb4b6e8e87c2e 🚀

--- index-d17e7c842be43f3c.js
+++ index-d17e7c842be43f3c.js
@@ -276,7 +276,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-64661c6b1bce00d9_bg.wasm', import.meta.url);
+        input = new URL('index-d17e7c842be43f3c_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
