42e0dcdf0726638c6238893abb63085d3f938531
io12 <7348004+io12@users.noreply.github.com>
2023-07-11 17:35:18 +0000

    Deploying to gh-pages from @ io12/yew-octicons@0d37ffe07ce2ea23a7b11d0c74eb7d9ecf994188 🚀

--- index-3e833bcafeb43f50.js
+++ index-3e833bcafeb43f50.js
@@ -637,7 +637,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('index-3695d321dd670e53_bg.wasm', import.meta.url);
+        input = new URL('index-3e833bcafeb43f50_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
